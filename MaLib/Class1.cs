97cd0e61a9d7c9c6d1fbc196811db23489b0f334
Julien Zana <julien.zana5@gmail.com>
2017-04-26 13:42:26 +0200

    validation1

---
+++ MaLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaLib
+{
+    public class MaClasse
+    {
+        internal static int MyAdd(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ PersonApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PersonApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> data = new List<string>();
+            data.Add("M");
+            data.Add("F");
+            Sexe.ItemsSource = data;
+            Sexe.SelectedIndex = 0;
+        }
+
+        private void Sexe_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string selected = e.AddedItems[0] as string;
+        }
+
+    }
+}
